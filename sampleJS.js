35c3920b137275f32b35e764f0d794df74088983
Gopika Chandran <gopika82174@gmail.com>
2021-02-25 17:30:27 +0530

    first commit

---
+++ sampleJS.js
@@ -0,0 +1,41 @@
+const signUpButton = document.getElementById('signUp');
+const button = document.getElementById('signIn');
+const container = document.getElementById('container');
+const form1=document.getElementById('form1');
+const form2=document.getElementById('form2');
+
+ document.getElementById("registerpg").onclick = myfunction() { 
+  
+            document.getElementById("login-form").style.display = "block"; 
+   document.getElementById("register-form").style.display = "none"; 
+        } 
+        
+/*("#registerpg").click(function() {
+ ("#login-form").hide();
+});*/
+/*function onclick(){
+    $("#register-form").hide();
+}*/
+/*function loginpg() {
+        document.getElementById("register-form").style.display = "block";
+        document.getElementById("login-form").style.display = "block;
+    }
+    function registerpg() {
+        document.getElementById("register-form").style.display = "none";
+        document.getElementById("login-form").style.display = "block";
+    }
+*/
+// document.getElementById("register-form").style.display="none";
+      
+    /* function showhide()
+    {  
+         var div = document.getElementById("container1");  
+         if (div.style.display !== "none") 
+         {  
+             div.style.display = "none";  
+         }  
+         else
+         {  
+             div.style.display = "block";  
+         }  
+    }  */
